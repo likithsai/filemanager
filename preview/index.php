a8e8cbf5941fda47c5ff88b47e1f2289e34eef82
likithsai <likithsai13@gmail.com>
2021-12-03 08:34:34 +0530

    Minor UI Changes
    1. Created Index page for preview
    2. Added linking logic for image, video and PDF
    3. Minor UI Fixes

--- preview/index.php
+++ preview/index.php
@@ -5,98 +5,51 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-    <style>
-        * {
-            font-family: 'Open Sans', sans-serif;
-            margin: 0;
-            box-sizing: border-box;
-        }
-
-        html {
-            scroll-snap-type: y mandatory;
-        }
-
-        body {
-            color: white;
-            background-color: black;
-            height: 100vh;
-            display: grid;
-            place-items: center;
-        }
-
-        video::-webkit-media-controls-fullscreen-button,
-        video::-webkit-media-controls-mute-button,
-        video::-webkit-media-controls-volume-slider,
-        video::-webkit-media-controls-overflow-menu-list-item {
-            display: none !important;
-        }
-
-        /* video {
-            scroll-snap-align: start;
-        } */
-    </style>
+    <title>Filemanager</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
 </head>
 
-<body style="background-color: #000;">
-    <div id="result_data" style="width: 100vw; height: 100vh;">
-        <div style="width: 100vw; height: 100vh; scroll-snap-align: start;">
-            <div style="position: sticky; padding: 15px; left: 0; background: rgba(0,0,0,0.5); display: flex; align-items: center; justify-content: space-between; width: 100%;">
-                <p style="font-size: 1em;">Sample Exaple</p>
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-tags" viewBox="0 0 16 16">
-                        <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
-                        <path d="M8 4a.5.5 0 0 1 .5.5V6H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V7H6a.5.5 0 0 1 0-1h1.5V4.5A.5.5 0 0 1 8 4z" />
-                    </svg>
+<body class="bg-light">
+    <main>
+        <div class="container py-1 py-md-4">
+            <div class="py-5 my-5 text-center container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-8 mx-auto">
+                        <div class="fw-light">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-images mb-3" viewBox="0 0 16 16">
+                                <path d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"></path>
+                            </svg>
+                            <h1 class="fw-bold">Filemanager</h1>
+                        </div>
+                        <p class="lead text-muted">Simple filemanager</p>
+                        <div class="col my-4">
+                            <a href="preview.php?type=video" class="btn btn-sm btn-primary">
+                                <i class="bi bi-film mr-5"></i>
+                                <span>Video</span>
+                            </a>
+                            <a href="preview.php?type=images" class="btn btn-sm btn-primary">
+                                <i class="bi bi-images mr-5"></i>
+                                <span>Images</span>
+                            </a>
+                            <a href="preview.php?type=pdf" class="btn btn-sm btn-primary">
+                                <i class="bi bi-file-earmark-pdf-fill mr-5"></i>
+                                <span>PDF</span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <video controls preload="auto" width="100%" height="90%">
-                <source src="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4">
-            </video>
         </div>
-    </div>
-    <script>
-        $(function() {
-            //  initial page count
-            var pagecount = 0;
-            var loadData = true;
+    </main>
 
-            function postDataToServer(url) {
-                $.ajax({
-                    url: url,
-                    type: 'POST',
-                    data: {
-                        page: pagecount
-                    },
-                    cache: false,
-                    success: function(result) {
-                        $("#result_data").append(result);
-                        if (result.length !== 0) {
-                            loadData = true;
-                        } else {
-                            loadData = false;
-                        }
-                    }
-                });
-            }
-
-            $(window).scroll(function() {
-                //  pause all videos
-                $('video').each(function() {
-                    $(this).get(0).pause();
-                });
-
-                if (document.body.scrollHeight - $(this).scrollTop() <= $(this).height() && loadData == true) {
-                    pagecount = parseInt(pagecount) + 1;
-                    postDataToServer('ajaxPreview.php');
-                }
-            });
-        });
-    </script>
+    <!-- <footer class="text-muted py-4 bg-white shadow-sm navbar-fixed-bottom">
+        <div class="container">
+            <p class="mb-1 text-center">&copy; likith sai</p>
+        </div>
+    </footer> -->
 </body>
-
 </html>
---
+++ preview/preview.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Open Sans', sans-serif;
+            margin: 0;
+            box-sizing: border-box;
+        }
+
+        html {
+            scroll-snap-type: y mandatory;
+        }
+
+        body {
+            color: white;
+            background-color: black;
+            height: 100vh;
+            display: grid;
+            place-items: center;
+        }
+
+        video::-webkit-media-controls-fullscreen-button,
+        video::-webkit-media-controls-mute-button,
+        video::-webkit-media-controls-volume-slider,
+        video::-webkit-media-controls-overflow-menu-list-item {
+            display: none !important;
+        }
+
+        /* video {
+            scroll-snap-align: start;
+        } */
+
+        @media screen and (min-width: 1024px) {
+            #result_data {
+                max-width: 768px;
+            }
+        }
+    </style>
+</head>
+
+<body style="background-color: #000;">
+    <div id="result_data" style="width: 100vw; height: 100vh;">
+        <div style="width: 100%; height: 100%; scroll-snap-align: start;">
+            <div style="position: sticky; padding: 15px; left: 0; background: rgba(0,0,0,0.5); display: flex; align-items: center; justify-content: space-between; width: 100%;">
+                <p style="font-size: 1em;">Sample Exaple</p>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-tags" viewBox="0 0 16 16">
+                        <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z" />
+                        <path d="M8 4a.5.5 0 0 1 .5.5V6H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V7H6a.5.5 0 0 1 0-1h1.5V4.5A.5.5 0 0 1 8 4z" />
+                    </svg>
+                </div>
+            </div>
+            <video controls preload="auto" width="100%" height="90%">
+                <source src="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4">
+            </video>
+        </div>
+    </div>
+    <script>
+        $(function() {
+            //  initial page count
+            var pagecount = 0;
+            var loadData = true;
+
+            function postDataToServer(url) {
+                $.ajax({
+                    url: url,
+                    type: 'POST',
+                    data: {
+                        page: pagecount,
+                        type: <?php  echo $_GET['type']; ?>
+                    },
+                    cache: false,
+                    success: function(result) {
+                        $("#result_data").append(result);
+                        if (result.length !== 0) {
+                            loadData = true;
+                        } else {
+                            loadData = false;
+                        }
+                    }
+                });
+            }
+
+            $(window).scroll(function() {
+                //  pause all videos
+                $('video').each(function() {
+                    $(this).get(0).pause();
+                });
+
+                if (document.body.scrollHeight - $(this).scrollTop() <= $(this).height() && loadData == true) {
+                    pagecount = parseInt(pagecount) + 1;
+                    postDataToServer('ajaxPreview.php');
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
