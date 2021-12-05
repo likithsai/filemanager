ad4784d03da0f828711815a0646868d0b6998a72
likithsai <likithsai13@gmail.com>
2021-12-05 10:07:42 +0530

    Added logics to filemanager

--- preview/filescanner.php
+++ preview/filescanner.php
@@ -1,4 +1,9 @@
 <?php
+
+    //  check if path is specified in the URL, if not use the default path
+    $path = (isset($_GET['path'])) ? urldecode($_GET['path']) : "../";
+
+    //  function to recursively scan files in the path
     function getDirContents($dir, $filter = '', &$results = array()) {
         $files = scandir($dir);
 
@@ -17,5 +22,22 @@
         return $results;
     } 
 
-    var_dump(getDirContents('../'));
+    if(isset($_GET['cmd'])) {
+        switch($_GET['cmd']) {
+            case 'getFiles' :
+                var_dump(getDirContents($path));
+                break;
+
+            case 'findDuplicates':
+                echo "find duplicates<br />path: " . $path;
+                break;
+
+            default:
+                echo "Command not found!";
+                break;
+        }
+    } else {
+        var_dump(getDirContents('../'));
+    }
+
 ?>
